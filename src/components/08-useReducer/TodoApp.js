a45a7013259381854558ecab1d0db29a14c4751e
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-07 22:36:19 -0500

    Toggle Todo - Marcar completado o pendiente un Todo

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -33,6 +33,13 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleToggle = (todoId) => {
+    dispatch({
+      type: "toggle",
+      payload: todoId,
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -63,7 +70,10 @@ export const TodoApp = () => {
           <ul className="list-group list-group-flush">
             {todos.map((todo, i) => (
               <li key={todo.id} className="list-group-item">
-                <p className="text-center">
+                <p
+                  className={`${todo.done && "complete"}`}
+                  onClick={() => handleToggle(todo.id)}
+                >
                   {i + 1}.{todo.desc}
                 </p>
                 <button
